0b3168562c5e50d26212c646769f9b6556082c9e
Kim Kyunghye <3939549@naver.com>
2017-05-23 14:54:41 +0900

    web.xml추가

---
+++ TeamProject_Movie/src/servlet/MovieMainServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class MovieMainServlet extends HttpServlet {
+	/**
+	 * 영화 메인페이지
+	 * 평균평점top1~5보여줌.
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
+}
